b61f130f9e8e6aaa7b8918c85893e4f692ccb145
Debi Darmawan <debi.darmawan@gpos.id>
2024-06-20 01:30:17 +0700

    add telegram module and add telegram notification

--- rumah-kirana-v2/rumah-kirana-v2.ino
+++ rumah-kirana-v2/rumah-kirana-v2.ino
@@ -1,21 +1,28 @@
 #include <WiFi.h>
-#include <NTPClient.h>
 #include <WiFiUdp.h>
-// #include <UniversalTelegramBot.h>
+#include <WiFiClientSecure.h>
+#include <NTPClient.h>
+#include <UniversalTelegramBot.h>
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "asia.pool.ntp.org", 25200); // 25200 represents the timezone offset in seconds for UTC+7
+NTPClient timeClient(ntpUDP, "asia.pool.ntp.org", 25200);  // 25200 represents the timezone offset in seconds for UTC+7
 
-const char* ssid       = "Kirana";
-const char* password   = "Anatri23";
-// const char* BOT_TOKEN = "5942587653:AAHBmInVp1q8tMX14hBw4u1DJKEFKl1P5Pg";
+const char* ssid = "Kirana";
+const char* password = "Anatri23";
+const char* BOT_TOKEN = "7271170210:AAHMSo9I0Cor6uVOv4igcbC_eEHcROUJFgo";
 const int lampPin = 5;
 bool lampOn = false;
 bool lampOff = false;
 
-// UniversalTelegramBot bot("5942587653:AAHBmInVp1q8tMX14hBw4u1DJKEFKl1P5Pg");
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOT_TOKEN, client);
 
-void setup(){
+String chatId = "641416700";
+
+int syncAttempts = 0;
+const int maxSyncAttempts = 3;  // Maximum number of synchronization attempts
+
+void setup() {
   Serial.begin(115200);
 
   //connect to WiFi
@@ -23,39 +30,61 @@ void setup(){
   Serial.println(ssid);
 
   WiFi.begin(ssid, password);
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
 
   while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
+    delay(500);
+    Serial.println(".");
   }
-  Serial.println(" CONNECTED");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("CONNECTED");
+  // Serial.println("IP address: ");
+  // Serial.println(WiFi.localIP());
 
   // Initialize NTP client
   timeClient.begin();
 
   // Ensure initial time synchronization
-  Serial.println("Synchronizing time with NTP server...");
-  while(!timeClient.update()) {
-    Serial.println("Failed to get time from NTP server, retrying...");
-    timeClient.forceUpdate();
-    delay(2000);
+  Serial.println("Synchronizing time with NTP server ");
+  if (!timeClient.update()) {
+    Serial.println("Failed to get time from NTP server, retrying ");
+    while (!timeClient.update()) {
+      Serial.println(".");
+      timeClient.forceUpdate();
+      delay(10000);
+
+      syncAttempts++;
+
+      // Check if retry limit exceeded
+      if (syncAttempts >= maxSyncAttempts) {
+        Serial.println("Failed to synchronize time after 30 seconds. Restarting ESP32...");
+        delay(5000);  // Delay for 5 second before restart
+
+        // Restart ESP32
+        ESP.restart();
+      }
+    }
   }
   Serial.println("Time synchronized successfully.");
 
   pinMode(lampPin, OUTPUT);
 
-  // bot.sendMessage("System started up!");
+  // bot.sendMessage(chatId, "System started up!");
+  bool messageSent = bot.sendMessage(chatId, "System started up!", "");
+
+  if (messageSent) {
+    Serial.println("Message sent successfully!");
+  } else {
+    Serial.println("Failed to send message. Check botToken, chatId, or network connection.");
+  }
 }
 
-void loop(){
+void loop() {
   timeClient.update();
 
   int hour = timeClient.getHours();
   int minute = timeClient.getMinutes();
 
-  Serial.println(timeClient.getFormattedTime());
+  // Serial.println(timeClient.getFormattedTime());
 
   if (hour < 6) {
     if (!lampOn) {
